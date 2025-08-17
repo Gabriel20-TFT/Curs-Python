7ec5b091883e46e0fdeee22f34c556d17b12d425
CatalinBrz98 <48378219+CatalinBrz98@users.noreply.github.com>
2025-08-17 15:09:19 +0300

    Sesiunea 17

---
+++ Session 17/Animale.py
@@ -0,0 +1,21 @@
+class Animal:
+    def __init__(self, nume_specie, varsta):
+        self.nume_specie = nume_specie
+        self.varsta = varsta
+
+    def __str__(self):
+        return f"Animal - ({self.nume_specie} - {self.varsta})"
+
+class Mamifer(Animal):
+    def __init__(self, nume_specie, varsta, picioare):
+        super().__init__(nume_specie, varsta)
+        self.picioare = picioare
+
+    def __str__(self):
+        return f"Mamifer - ({self.nume_specie} - {self.varsta} - {self.picioare})"
+
+if __name__ == "__main__":
+    # anumal1 = Animal("Homo Sapiens", 26)
+    # print(anumal1)
+    mamifer1 = Mamifer("Homo Sapiens", 26, 2)
+    print(mamifer1)
