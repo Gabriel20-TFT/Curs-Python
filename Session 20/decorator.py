6a7a895c483364dfa2b0f229e91eebbb1474d03d
CatalinBrz98 <48378219+CatalinBrz98@users.noreply.github.com>
2025-08-28 09:18:17 +0300

    Sesiunea 20

--- Session 20/decorator.py
+++ Session 20/decorator.py
@@ -39,15 +39,24 @@ def factorial(n):
 def suma_numere(*args):
     return sum(args)
 
+
 @list_decorator
 def get_n_list(n):
     return list(range(1, n + 1))
 
 
 if __name__ == '__main__':
-    print(factorial(50))
+    # suma_n_with_timer = timer_decorator(suma_n)
+    # factorial_with_timer = timer_decorator(factorial)
+
     print(suma_n(50000000))
+    print(suma_n(124141))
+    print(suma_n(12412412))
+    print(suma_n(2141))
+    print(suma_n(4123145))
+    print(factorial(1))
+    print(factorial(500))
+    print(suma_n(500))
+    print(factorial(50))
 
-    print(suma_numere(1, 3, 6, 8, 2))
-
-    print(get_n_list(50))
+    # print(suma_n_with_timer(50000000))
